d956fd3a41d43f49b64567a28808fe1df320e75a
galuhyeager23 <galuh.garhadi408@gmail.com>
2025-05-26 02:33:54 +0700

    delete

--- backend/utils/generateOtp.js
+++
@@ -1,9 +0,0 @@
-function generateOtp(length = 6) {
-  let otp = '';
-  for (let i = 0; i < length; i++) {
-    otp += Math.floor(Math.random() * 10); // angka 0-9
-  }
-  return otp;
-}
-
-module.exports = generateOtp;
